f6ffc63037b278196c44ad3e82d89e172f4f2d31
Joanne Lin <joannelin@Joannes-Mac.local>
2023-02-02 03:00:13 -0600

    normalize

--- intensity_normalize.py
+++ intensity_normalize.py
@@ -1,25 +1,33 @@
+from locale import normalize
 import numpy as np
 import cv2
 # import matplotlib.pyplot as plt
 import os
-# "/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs"
 
-def image_dataT1(folder):
+def image_dataT1(folder, type):
     images = []
     for patientNumber in os.listdir(folder):
         if not patientNumber.startswith('.'):
             for image_type in os.listdir(folder+"/"+patientNumber):
-                
-                if image_type == "T1":
+                if image_type == type:
                     print(image_type)
-                    print("yay1")
                     for last_folder in os.listdir(folder+"/"+patientNumber+"/"+image_type):
-                        if os.path.isfile(os.path.join(folder+"/"+patientNumber+"/"+image_type, last_folder)):
-                            img = cv2.imread(os.path.join(folder,patientNumber,image_type,last_folder))
-                            if img is not None:
-                                images.append(img)
-                                print("yay")
+                        if not last_folder.startswith('.'):
+                            for slices in os.listdir(folder+"/"+patientNumber+"/"+image_type+"/"+last_folder):
+
+                                img = cv2.imread(os.path.join(folder,patientNumber,image_type,last_folder,slices))
+                                print(img)
+                                if img is not None:
+                                    normalizedimage = cv2.normalize(img,None, 0, 255, cv2.NORM_MINMAX)
+                                    cv2.imshow('Normalized_image', normalizedimage)
+                                    cv2.waitKey(0)
+                                    cv2.destroyAllWindows()
+                                    
+                                    # writeToCSV(path,patientNumber,image_type,xxx,yyy,zzz)
+                                    # saveImage(path,"",nornalizedImage)
+                                    # images.append(img)
+                                    print("hehe")
     return images
 
-image_dataT1("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs")
+image_dataT1("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T1")
 # # cv2.imread(path, cv2.IMREAD_GRAYSCALE)
