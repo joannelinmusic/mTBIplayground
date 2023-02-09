813dd047e8bff9915075c9ab6a98d441e868ac81
Joanne Lin <joannelin@lpc-depaulsecure-203-236.depaulsecure-student.depaul.edu>
2023-02-09 13:12:41 -0600

    modified

--- intensity_normalize.py
+++ intensity_normalize.py
@@ -3,8 +3,11 @@ import numpy as np
 import cv2
 # import matplotlib.pyplot as plt
 import os
+import csv
 
-def image_dataT1(folder, type):
+
+# Normalizing images by type, running once will be only one type
+def normalize_image(folder, type):
     images = []
     for patientNumber in os.listdir(folder):
         if not patientNumber.startswith('.'):
@@ -16,18 +19,27 @@ def image_dataT1(folder, type):
                             for slices in os.listdir(folder+"/"+patientNumber+"/"+image_type+"/"+last_folder):
 
                                 img = cv2.imread(os.path.join(folder,patientNumber,image_type,last_folder,slices))
-                                print(img)
+                                
                                 if img is not None:
+                                    
+                                    print(np.amin(img), np.amax(img))
                                     normalizedimage = cv2.normalize(img,None, 0, 255, cv2.NORM_MINMAX)
-                                    cv2.imshow('Normalized_image', normalizedimage)
-                                    cv2.waitKey(0)
-                                    cv2.destroyAllWindows()
+                                    
+                                    
+                                    # Command lines to print out the min and max intensity
+                                    # cv2.imshow('Normalized_image', normalizedimage)
+                                    # cv2.waitKey(0)
+                                    # cv2.destroyAllWindows()
+
+                                    # print(np.amin(normalizedimage), np.amax(normalizedimage))
                                     
                                     # writeToCSV(path,patientNumber,image_type,xxx,yyy,zzz)
                                     # saveImage(path,"",nornalizedImage)
                                     # images.append(img)
-                                    print("hehe")
+                                    
     return images
 
-image_dataT1("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T1")
-# # cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+# normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T1")
+# normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T2")
+normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "FLAIR")
+
