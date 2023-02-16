bf023a53c8922a21acfcf7e796a621a89f0a3cb8
Joanne Lin <joannelin@Joannes-Mac.local>
2023-02-16 00:07:37 -0600

    done

--- intensity_normalize.py
+++ intensity_normalize.py
@@ -18,7 +18,6 @@ def normalize_image(folder, type):
             if not patientNumber.startswith('.'):
                 for image_type in os.listdir(folder+"/"+patientNumber):
                     if image_type == type:
-                        print(image_type)
                         for last_folder in os.listdir(folder+"/"+patientNumber+"/"+image_type):
                             if not last_folder.startswith('.'):
                                 for slices in os.listdir(folder+"/"+patientNumber+"/"+image_type+"/"+last_folder):
@@ -27,13 +26,14 @@ def normalize_image(folder, type):
                                     
                                     if img is not None:
                                         
-                                        print(patientNumber, slices, np.amin(img), np.amax(img))
+                                        
                                         normalizedimage = cv2.normalize(img,None, 0, 255, cv2.NORM_MINMAX)
                                         if np.amin(img)!=np.amax(img) and np.mean(img) != 0:
                                             myWriter.writerow({'patientID': patientNumber, 'image filename': slices, \
                                                 'Min Intensity(before)': np.amin(img), 'Max Intensity(before)': np.amax(img), \
                                                 'Min Intensity(after)': np.amin(normalizedimage), 'Max Intensity(after)':np.amax(normalizedimage)})
                                             
+                                        images.append(img)
                                         # Command lines to print out the min and max intensity
                                         # cv2.imshow('Normalized_image', normalizedimage)
                                         # cv2.waitKey(0)
@@ -43,11 +43,15 @@ def normalize_image(folder, type):
                                         
                                         # writeToCSV(path,patientNumber,image_type,xxx,yyy,zzz)
                                         # saveImage(path,"",nornalizedImage)
-                                        # images.append(img)
-                                    
+                                        # images.append(img)                          
+    
     return images
 
-# normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T1")
-# normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T2")
-normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "FLAIR")
 
+def main():
+    # normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T1")
+    # normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "T2")
+    normalize_image("/Users/joannelin/Desktop/Motorola/mTBIplayground/Datasets/mTBI_data_new_2022/mTBI_Data_JPEGs", "FLAIR")
+
+if __name__ == "__main__":
+    main()
